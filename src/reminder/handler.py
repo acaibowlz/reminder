62600b31527b8eac6d89433b7463105af74a3927
hengtseChou <hankthedev@gmail.com>
2025-08-23 11:30:07 +0800

    fix: add auth headers to profile api

--- src/reminder/handler.py
+++ src/reminder/handler.py
@@ -5,6 +5,7 @@ import requests
 from linebot.v3 import WebhookHandler
 from linebot.v3.messaging import ApiClient, Configuration, MessagingApi, ReplyMessageRequest, TextMessage
 from linebot.v3.webhooks import FollowEvent, MessageEvent, TextMessageContent, UnfollowEvent
+from requests.api import head
 
 from reminder.const import DATABASE_URL, LINE_CHANNEL_ACCESS_TOKEN, LINE_CHANNEL_SECRET
 from reminder.db import add_user, delete_user
@@ -18,10 +19,15 @@ handler = WebhookHandler(LINE_CHANNEL_SECRET)
 @handler.add(FollowEvent)
 def handle_follow_event(event: FollowEvent) -> None:
     user_id = event.source.user_id
-    resp = requests.get(f"https://api.line.me/v2/bot/profile/{user_id}")
+    resp = requests.get(
+        f"https://api.line.me/v2/bot/profile/{user_id}",
+        headers={"Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"},
+    )
     user_info = resp.json()
     display_name = user_info.get("displayName")
     picture_url = user_info.get("pictureUrl")
+    logger.info(f"Added (or unblocked) by: {user_id}")
+    logger.info(f"Display name: {display_name}")
 
     with psycopg.connect(conninfo=DATABASE_URL) as conn:
         add_user(user_id=user_id, display_name=display_name, picture_url=picture_url, conn=conn)
@@ -31,17 +37,14 @@ def handle_follow_event(event: FollowEvent) -> None:
             ReplyMessageRequest(reply_token=event.reply_token, messages=[TextMessage(text="hello my new friend!")])
         )
 
-    logger.info(f"Added (or unblocked) by: {user_id}")
-    logger.info(f"Display name: {display_name}")
-
 
 @handler.add(UnfollowEvent)
 def handle_unfollow_event(event: UnfollowEvent) -> None:
     user_id = event.source.user_id
+    logger.info(f"Blocked by: {user_id}")
+
     with psycopg.connect(conninfo=DATABASE_URL) as conn:
         delete_user(user_id=user_id, conn=conn)
-
-    logger.info(f"Blocked by: {user_id}")
 
 
 def get_reply_message(msg: str, user_id: str):
